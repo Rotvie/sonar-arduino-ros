0b9743c526aa6159e92756a4b1de471d9f82aa67
Ricardo HK <76816272+Rotvie@users.noreply.github.com>
2021-12-24 00:26:04 -0500

    Publish Sonar Data

---
+++ sonar-arduino-ros.ino
@@ -0,0 +1,55 @@
+#include <ros.h>
+#include <ros/time.h>
+#include <sensor_msgs/Range.h>
+
+#define echoPin 2 // attach pin D2 Arduino to pin Echo of HC-SR04
+#define trigPin 3 //attach pin D3 Arduino to pin Trig of HC-SR04
+
+// defines variables
+long duration; // variable for the duration of sound wave travel
+int distance; // variable for the distance measurement
+
+ros::NodeHandle nh; //create an object which represents the ROS node.
+
+void sensor_msg_init(sensor_msgs::Range &range_name, char *frame_id_name)
+{
+  range_name.radiation_type = sensor_msgs::Range::ULTRASOUND;
+  range_name.header.frame_id = frame_id_name;
+  range_name.field_of_view = 0.26;
+  range_name.min_range = 0.0;
+  range_name.max_range = 2.0;
+}
+
+//Create three instances for range messages.
+sensor_msgs::Range range_center;
+
+//Create publisher onjects for all sensors
+ros::Publisher pub_range_center("/ultrasound_center", &range_center);
+
+void setup() {
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
+  nh.initNode();
+  nh.advertise(pub_range_center);
+  sensor_msg_init(range_center, "/ultrasound_center");
+  Serial.begin(57600); // // Serial Communication is starting with 9600 of baudrate speed
+}
+
+void loop() {
+  // Clears the trigPin condition
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+
+  range_center.range = distance;
+  range_center.header.stamp = nh.now();
+  pub_range_center.publish(&range_center);
+  nh.spinOnce();//Handle ROS events
+}
